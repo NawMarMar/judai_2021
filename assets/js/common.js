3a45274fe1288cb5a0dcca64a24d1f48188b9a5b
NawMarMar <68273064+NawMarMar@users.noreply.github.com>
2021-01-05 16:27:32 +0630

    update main visual

---
+++ assets/js/common.js
@@ -0,0 +1,12 @@
+// slider
+$('.slider').slick({
+    autoplay: false,
+    dots: true,
+    arrows: true,
+    centerMode: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 1200,
+    variableWidth: false,
+});
