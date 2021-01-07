750b5e66b2dbb2af7c45dcdddd40b12284425810
NawMarMar <68273064+NawMarMar@users.noreply.github.com>
2021-01-07 14:01:43 +0630

    update design

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,35 +9,36 @@ $('.slider').slick({
     infinite: true,
     speed: 1200,
     variableWidth: false,
+    fade: true,
 });
 // go to top
-jQuery(function($) {
-    $(".ft-top").hide();
-    $(window).on("scroll", function() {
-        if ($(this).scrollTop() > 800) {
-            $(".ft-top").fadeIn("fast");
-        } else {
-            $(".ft-top").fadeOut("fast");
-        }
-        scrollHeight = $(document).height();
-        scrollPosition = $(window).height() + $(window).scrollTop();
-        footHeight = $("footer").innerHeight();
-        if (scrollHeight - scrollPosition <= footHeight) {
-            $(".ft-top").css({
-                "position": "absolute",
-                "bottom": footHeight - 430
-            });
-        } else {
-            $(".ft-top").css({
-                "position": "fixed",
-                "bottom": "30px"
-            });
-        }
-    });
-    $('.top-btn').click(function() {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 400);
-        return false;
-    });
-});
+// jQuery(function($) {
+//     $(".ft-top").hide();
+//     $(window).on("scroll", function() {
+//         if ($(this).scrollTop() > 800) {
+//             $(".ft-top").fadeIn("fast");
+//         } else {
+//             $(".ft-top").fadeOut("fast");
+//         }
+//         scrollHeight = $(document).height();
+//         scrollPosition = $(window).height() + $(window).scrollTop();
+//         footHeight = $("footer").innerHeight();
+//         if (scrollHeight - scrollPosition <= footHeight) {
+//             $(".ft-top").css({
+//                 "position": "absolute",
+//                 "bottom": footHeight - 430
+//             });
+//         } else {
+//             $(".ft-top").css({
+//                 "position": "fixed",
+//                 "bottom": "30px"
+//             });
+//         }
+//     });
+//     $('.top-btn').click(function() {
+//         $('body,html').animate({
+//             scrollTop: 0
+//         }, 400);
+//         return false;
+//     });
+// });
