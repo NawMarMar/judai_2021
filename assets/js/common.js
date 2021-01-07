b2d113cdb2215ffbb24a6ed3cf80337baaa140e8
NawMarMar <68273064+NawMarMar@users.noreply.github.com>
2021-01-07 11:42:19 +0630

    update design

--- assets/js/common.js
+++ assets/js/common.js
@@ -10,3 +10,34 @@ $('.slider').slick({
     speed: 1200,
     variableWidth: false,
 });
+// go to top
+jQuery(function($) {
+    $(".ft-top").hide();
+    $(window).on("scroll", function() {
+        if ($(this).scrollTop() > 800) {
+            $(".ft-top").fadeIn("fast");
+        } else {
+            $(".ft-top").fadeOut("fast");
+        }
+        scrollHeight = $(document).height();
+        scrollPosition = $(window).height() + $(window).scrollTop();
+        footHeight = $("footer").innerHeight();
+        if (scrollHeight - scrollPosition <= footHeight) {
+            $(".ft-top").css({
+                "position": "absolute",
+                "bottom": footHeight - 430
+            });
+        } else {
+            $(".ft-top").css({
+                "position": "fixed",
+                "bottom": "30px"
+            });
+        }
+    });
+    $('.top-btn').click(function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
